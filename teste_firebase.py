dcfbc5cbd74f440f94d443617e83b57ec9b1db3a
Vinicius França <silva.franca@acad.ifma.edu.br>
2023-04-14 09:55:50 -0300

    Update teste_firebase.py
    mudar o nome do arquivo para firebase_sites.py

--- teste_firebase.py
+++ teste_firebase.py
@@ -1,15 +1,45 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import io
 
 cred = credentials.Certificate("./key/web-links-b05c4-firebase-adminsdk-dmkbz-2b90fced38.json")
+
 firebase_admin.initialize_app(cred)
 
 
 db = firestore.client()
 
-sites_ref = db.collection(u'sites_info')
-docs = sites_ref.stream()
 
-for doc in docs:
-    print(f'${doc.id} => {doc.to_dict()}')
+def salvarSite(url):
+    _, data = db.collection("sites").add({
+    "site": url,
+        })
+    return data.id
+ 
+        
+
+def salvarBody(id, content):
+       db.collection("sites").document(id).collection("info").add({
+        "type": "body",
+        "content": convertBlob(content)
+        })
+ 
+
+def salvarMetaTags(id, tags):
+        for tag in tags:
+            
+            db.collection("sites").document(id).collection("info").add({
+            "type": tag.name,
+            "content": convertBlob(tag.text)
+             })
+        
+
+def convertBlob(content):
+        blob = io.BytesIO()
+        blob.write(b"{content}")
+        return blob.getvalue()
+            
+
+
+
